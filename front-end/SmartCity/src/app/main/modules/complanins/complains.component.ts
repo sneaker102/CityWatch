eba67c17d78c4a6377876776c4d49a48d61607b4
Lucian Ciolac <ciolac.lucian@yahoo.com>
2019-12-14 16:26:15 +0200

    a

--- front-end/SmartCity/src/app/main/modules/complanins/complains.component.ts
+++ front-end/SmartCity/src/app/main/modules/complanins/complains.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import { latLng, tileLayer, circle, polygon, marker } from 'leaflet';
+import { faMapMarkedAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-complains',
@@ -7,8 +8,10 @@ import { latLng, tileLayer } from 'leaflet';
   styleUrls: ['./complains.component.scss']
 })
 export class ComplainsComponent implements OnInit {
+  public faMapMarkedAlt = faMapMarkedAlt;
+  public faBars = faBars;
 
-  options = {
+  public options = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
@@ -18,10 +21,30 @@ export class ComplainsComponent implements OnInit {
     center: latLng([ 45.66, 25.61 ])
   };
 
+  public layers = [
+    circle([ 45.66, 25.61 ], { radius: 4000 }),
+    // polygon([[ 45.66, 25.61 ], [ 46.92, -121.92 ], [ 46.87, -121.8 ]]),
+    marker([ 45.66, 25.63 ]).bindPopup('Tulai doamne')
+];
+
+
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  private addMarkerToMap(latlng, payload?): void {
+    this.layers.push(
+      marker(latlng)
+        .bindPopup(payload)
+    )
+  }
+
+  public clickOnMap(event): void {
+    console.log(event);
+    this.addMarkerToMap(event.latlng);
   }
 
 }
